378ab7d45aa25c7dc5c4db6980ce71665a72ce02
Wee Tien Guan <tg_wee@yahoo.com.sg>
2022-02-24 11:39:01 +0800

    radio event handling
    figured out how to handle radio buttons on the fly

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+document.addEventListener("change", (event) => {
+  console.log(event.target);
+  console.log(event.target.type);
+  console.log(event.target.value);
 
+  //   if (event.target.type == "radio") {
+  //     console.log(event.target.value);
+  //   }
+  //   if (event.target.id == "userEnterRate") {
+  //     console.log("user rate: ");
+  //     console.log(event.target.value);
+  //   }
+});
